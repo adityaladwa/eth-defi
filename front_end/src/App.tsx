65e543ca61a9276651e94fcd64b3e07c72f708fc
Aditya Ladwa <ladwa.aditya@gmail.com>
2021-10-16 20:12:50 +0530

    Created Header component to connect to wallet

--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Container } from '@material-ui/core'
+import { ChainId, DAppProvider } from '@usedapp/core'
+import React from 'react'
+import { Header } from './components/Header'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <DAppProvider
+      config={{
+        supportedChains: [ChainId.Rinkeby],
+      }}
+    >
+      <Header />
+      <Container maxWidth="md">
+        <div className="App">Hi.</div>
+      </Container>
+    </DAppProvider>
+  )
 }
 
-export default App;
+export default App
---
+++ front_end/src/components/Header.tsx
@@ -0,0 +1,36 @@
+import { Button, makeStyles } from '@material-ui/core'
+import { useEthers } from '@usedapp/core'
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    padding: theme.spacing(4),
+    display: 'flex',
+    justifyContent: 'flex-end',
+    gap: theme.spacing(1),
+  },
+}))
+export const Header = () => {
+  const classes = useStyles()
+  const { account, activateBrowserWallet, deactivate } = useEthers()
+  const isConnected = account !== undefined
+
+  return (
+    <div className={classes.container}>
+      <div>
+        {isConnected ? (
+          <Button color="primary" onClick={deactivate} variant="contained">
+            Disconnect
+          </Button>
+        ) : (
+          <Button
+            color="primary"
+            variant="contained"
+            onClick={() => activateBrowserWallet()}
+          >
+            Connect
+          </Button>
+        )}
+      </div>
+    </div>
+  )
+}
